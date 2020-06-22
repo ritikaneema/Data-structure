3671ecffda13f99f766de8d9f42fe51ff8afef31
ritikaneema <65679700+ritikaneema@users.noreply.github.com>
2020-06-22 22:15:31 +0530

    add optimization bubble sort
    add python implementation of optimize bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,10 +3,13 @@
 def bubbleSort(arr):
     n=len(arr)#lenght of array
     for i in range(n):#for all array elements :value of i denote no of sorted arr
+        flag=0
         for j in range(0,n-1-i):# last i element already in place
             if arr[j]>arr[j+1]:# check element with the next in the arr
                 arr[j],arr[j+1]=arr[j+1],arr[j]# swapping if the element is greater thean other element
-
+                flag=1
+        if flag==0:
+            break
 
 arr =[2,7,4,1,5,3]
 bubbleSort(arr)
