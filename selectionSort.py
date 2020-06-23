4b73d2af430d8131dcb57c6c5a58fc270dd0fd61
ritikaneema <65679700+ritikaneema@users.noreply.github.com>
2020-06-23 15:04:34 +0530

    add file selectionsort.py
    add selection sort implementation in python

--- selectionSort.py
+++ selectionSort.py
@@ -1,16 +1,16 @@
 #selectionSort
 def selectionSort(arr):
-     n=len(arr)
+     n=len(arr) #length of array
      for i in range (n):
          min_i=i
-         for j in range(i+1,n):
+         for j in range(i+1,n): #i is min then check from i+1
              if arr[min_i]>arr[j]:
                  min_i=j
-         arr[i],arr[min_i]=arr[min_i],arr[i]
+         arr[i],arr[min_i]=arr[min_i],arr[i] # if min_i is greater than other then swapping
 
 arr=[28,77,85,14,45,43]
-selectionSort(arr)
+selectionSort(arr) #call function
 
 print("Sorted array is:")
-for i in range(len(arr)):
+for i in range(len(arr)): #print array elements
     print("%d" %arr[i])
